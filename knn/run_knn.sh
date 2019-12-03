0de820c48aa5163a2e5eba17a2a2906b3396cb5e
allenyummy <chiangyulun0914@gmail.com>
2019-12-03 05:21:25 +0000

    revise data dir

--- knn/run_knn.sh
+++ knn/run_knn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-data_dir=data/
+data_dir=../data/
 train_valid_file=Data_train_validation_2019-1126.xlsx
 test_file=Data_test_2019-1126.xlsx
 feature_in='A1 A2 A3 A4 A5 A6 A7 A8 A9 A10 A11 A12 A13 A14 A15 A16 A17'
